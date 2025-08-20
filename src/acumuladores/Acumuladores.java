bd0c1c230d4f03e6b35a8edecdc7a4166a028f1c
Gabriel Acosta <acostagabriel126@gmail.com>
2025-08-20 13:20:10 -0300

    Ejercicio 1 y 2

--- src/acumuladores/Acumuladores.java
+++ src/acumuladores/Acumuladores.java
@@ -3,9 +3,8 @@ package acumuladores;
 public class Acumuladores {
 
 	/**
-	 * Dada una matriz de enteros y un número, verifica si existe alguna fila 
-	 * donde todos sus elementos sean múltiplos del número recibido por 
-	 * parámetro.
+	 * Dada una matriz de enteros y un número, verifica si existe alguna fila donde
+	 * todos sus elementos sean múltiplos del número recibido por parámetro.
 	 * 
 	 * Si la matriz está vacía o si el número no es positivo, devuelve falso.
 	 * 
@@ -13,53 +12,86 @@ public class Acumuladores {
 	 * @param num
 	 * @return
 	 */
-	public boolean todosMultiplosEnAlgunaFila(int[][] mat, int num) { 
-		throw new RuntimeException("Metodo no implementado aun!!!");
+	public boolean todosMultiplosEnAlgunaFila(int[][] mat, int num) {
+		if (mat == null || num <= 0)
+			return false;
+		boolean algunaFilaMultiplo = false;
+		for (int i = 0; i < mat.length; i++) {
+			boolean filaMultiplo = true;
+			for (int j = 0; j < mat[0].length; j++) {
+				filaMultiplo = filaMultiplo && mat[i][j] % num == 0;
+			}
+			algunaFilaMultiplo = algunaFilaMultiplo || filaMultiplo;
+		}
+		return algunaFilaMultiplo;
 	}
-	
+
 	/**
 	 * Dado 2 matrices se verifica si hay intersección entre las filas de cada
 	 * matriz, fila a fila.
 	 * 
-	 * Si las matrices tienen distinta cantidad de filas o si alguna matriz 
-	 * está vacía, devuelve falso.
+	 * Si las matrices tienen distinta cantidad de filas o si alguna matriz está
+	 * vacía, devuelve falso.
 	 * 
 	 * @param mat1
 	 * @param mat2
 	 * @return
 	 */
-	public boolean hayInterseccionPorFila(int[][] mat1, int[][]mat2) { 
-		throw new RuntimeException("Metodo no implementado aun!!!");
+	public boolean hayInterseccionPorFila(int[][] mat1, int[][] mat2) {
+		if (mat1 == null || mat2 == null || mat1.length == 0 || mat2.length == 0 || mat1.length != mat2.length) {
+			return false;
+		}
+		boolean interseccionEnTodas = true;
+		for (int i = 0; i < mat2.length; i++) {
+			boolean interccionFila = false;
+			for (int j = 0; j < mat1[0].length; j++) {
+				interccionFila = interccionFila || hayInterseccionEnFila(mat1[i][j], mat2[i]);
+			}
+			System.out.println(" interccionFila " + interccionFila);
+			interseccionEnTodas = interseccionEnTodas && interccionFila;
+			System.out.println(" interseccionEnTodas " + interseccionEnTodas);
+
+		}
+
+		return interseccionEnTodas;
 	}
-	
+
+	private boolean hayInterseccionEnFila(int mat1, int[] mat2) {
+		boolean interseccionEnFila = false;
+		for (int i = 0; i < mat2.length; i++) {
+			interseccionEnFila = interseccionEnFila || mat2[i] == mat1;
+		}
+		return interseccionEnFila;
+	}
+
 	/**
-	 * Dada una matriz y el índice de una columna, se verifica si existe alguna
-	 * fila cuya suma de todos sus elementos sea mayor estricto que la suma de
-	 * todos los elementos de la columna indicada por parámetro.
+	 * Dada una matriz y el índice de una columna, se verifica si existe alguna fila
+	 * cuya suma de todos sus elementos sea mayor estricto que la suma de todos los
+	 * elementos de la columna indicada por parámetro.
 	 * 
-	 * Si el índice de la columna es inválido o la matriz está vacía, devuelve 
+	 * Si el índice de la columna es inválido o la matriz está vacía, devuelve
 	 * falso.
 	 * 
 	 * @param mat
 	 * @param nColum
 	 * @return
 	 */
-	public boolean algunaFilaSumaMasQueLaColumna(int[][] mat, int nColum) { 
+	public boolean algunaFilaSumaMasQueLaColumna(int[][] mat, int nColum) {
 		throw new RuntimeException("Metodo no implementado aun!!!");
 	}
-	
+
 	/**
-	 * Dadas 2 matrices, se verifica si hay intersección entre las columnas de
-	 * cada matriz, columna a columna.
+	 * Dadas 2 matrices, se verifica si hay intersección entre las columnas de cada
+	 * matriz, columna a columna.
 	 * 
-	 * Si las matrices tienen distinta cantidad de columnas o alguna matriz 
-	 * está vacía, devuelve falso. 
+	 * Si las matrices tienen distinta cantidad de columnas o alguna matriz está
+	 * vacía, devuelve falso.
 	 * 
 	 * @param mat1
 	 * @param mat2
 	 * @return
 	 */
-	public boolean hayInterseccionPorColumna(int[][] mat1, int[][]mat2) { 
+	public boolean hayInterseccionPorColumna(int[][] mat1, int[][] mat2) {
 		throw new RuntimeException("Metodo no implementado aun!!!");
 	}
 }
